20184f87136a6c522aa1316c599685e5f407dd33
Maria Jose <mjsilva890@misena.edu.co>
2023-08-16 10:15:37 -0500

    commit form changes

---
+++ src/form/FormLog.jsx
@@ -0,0 +1,37 @@
+import { useForm } from "react-hook-form";
+
+const Formulario_log =() => {
+
+    const{register, handleSubmit}= useForm();
+    return <div classcontainer="container"><div class="card">
+        <div class="logos">
+        <image class="logo-horizontal" id="web-asiste" src="../resources/images/_logo_asiste_horizontal.png" alt="Logo de asiste con letras horizontales" />
+        <image class="logo-verde" id="web-sena" src="../resources/images/_logo_sena_verde.png" alt="Logo del SENA verde" style="max-width: 8rem;"/>
+        </div>
+                            
+        <h1 class="text-center texto">INGRESO DE USUARIOS REGISTRADOS</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>Número de documento</label>
+            <input type="text"  placeholder="Ingrese número de documento" {...register('document', {required: true})}/>
+            {appendErrors.document?.type === 'required' && <p>Este campo es requerido</p>}
+
+
+            <label>Contraseña</label>
+            <input type="password" name="password" placeholder="Ingrese número contraseña" {...register('password',{required: true})}/>
+            {appendErrors.password?.type === 'required' && <p>Este campo es requerido</p>}
+
+
+            <div class="button" />
+                    <button class="butt" id="butt">Iniciar</button>
+
+
+        </form>
+        <div class="olvido">
+                                <a href="../forgot/forgot.html">Olvide mi contraseña</a>
+                            </div>
+    </div>
+    </div>
+    
+}
+
+export default Formulario_log;
---
+++ src/form/LoginForm.js
@@ -0,0 +1,126 @@
+
+
+
+
+
+
+
+
+/*<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <title>Recuperación de contraseña</title>
+    <link rel="stylesheet" href="forgot.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="shortcut icon" href="../resources/images/triangulo.png" type="image/x-icon">
+</head>
+
+<body class="p-2 m-0 border-0 bd-example">
+    <div class="">
+        <div class="card container-fluid mb-3 bg-white">
+<body class="p-2 m-0 border-0 bd-example ">
+    <div class="first">
+        <div class="card mb-3 bg-white col-8">
+            <div class="row g-0">
+                <div class="col-md-6">
+                    <div class="all-conten d-flex justify-content-evenly">
+                        <img class="logo-asiste-horizontal" id="sitio-web-asiste" src="../resources/images/_logo_asiste_horizontal.png"
+                            style="max-width: 80%;" alt="Logo de asiste con letras horizontales">
+                <div class="col-md-6 form">
+                    <div class="logos">
+                        <img class="logo-asiste-horizontal" id="sitio-web-asiste"
+                            src="../resources/images/_logo_asiste_horizontal.png"
+                            alt="Logo de asiste con letras horizontales">
+                        <img class="logo-sena-verde" id="sitio-web-sena" src="../resources/images/_logo_sena_verde.png"
+                            alt="Logo del SENA verde" style="max-width: 8rem;">
+                            alt="Logo del SENA verde">
+                    </div>
+                    <div class="card-body">
+                        <h1 class="text-center texto">INGRESO DE USUARIOS REGISTRADOS</h1>
+                        <div class="labeles">
+                            <div class="input-group mb-3 desplegable">
+                                <label class="input-group-text" for="inputGroupSelect01" style="background-color:white; border: none;">
+                                    <img src="../resources/images/_identification_card.png" alt="Icono de documento"
+                                        style="max-height: 50px;">
+                                </label>
+                                <select class="form-select" id="inputGroupSelect01" style="font-size: 20px; border: none; margin-top: 20px;">
+                            <div class="input-group mb-3">
+                                <select class="form-select" id="inputGroupSelect01">
+                                    <option value="CC">Cédula de Ciudadania</option>
+                                    <option value="TI">Tarjeta de Identidad</option>
+                                    <option value="CE">Cédula de Extrangeria</option>
+                                    <option value="PEP">PEP</option>
+                                </select>
+                            </div>
+
+                            <div class="input-group input-group-lg">
+                                <span class="input-group-text" id="basic-addon1"
+                                    style="background-color: white; border-color: white;"><img
+                                        src="../resources/images/_user_picture.png" alt="User image"
+                                        style="max-width: 50px;"></span>
+                                <div class="form-floating">
+                                    <input type="text" class="form-control form-user" id="user" placeholder="Username"
+                                        aria-label="Username" aria-describedby="basic-addon1"
+                                        style="border: none;     border-bottom: 2px solid darkgrey">
+                                    <label for="userName" style="font-size: 20px; color: darkgray;">Numero de documento</label>
+                                        aria-label="Username" aria-describedby="basic-addon1">
+                                    <label for="userName">Número de documento</label>
+                                </div>
+                            </div>
+                            <div class="input-group input-group-lg">
+                                <span class="input-group-text" id="basic-addon1"
+                                    style="background-color: white; border-color: white;"><img
+                                        src="../resources/images/_password_picture.png" alt="User image"
+                                        style="max-width: 50px;"></span>
+                                <div class="form-floating">
+                                    <input type="password" class="form-control form-user" id="password"
+                                        placeholder="Contraseña" aria-label="Contraseña" aria-describedby="basic-addon1"
+                                        style="border: none; border-bottom: 2px solid darkgrey">
+                                    <label for="password" style="font-size: 20px; color: darkgray;">Contraseña</label>
+                                <div class="input-group input-group-lg">
+                                    <div class="form-floating">
+                                        <input type="password" class="form-control form-user" id="password"
+                                            placeholder="Contraseña" aria-label="Contraseña"
+                                            aria-describedby="basic-addon1">
+                                        <label for="password">Contraseña</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text1">
+                                <a href="../forgot/forgot.html">Olvide mi contraseña</a>
+                            </div>
+                        </div>
+                        <div class="button">
+                            <button class="btn registrer" id="btn">Registrarse</button>
+                            <button class="btn" id="btn">Iniciar</button>
+                        </div>
+                        <div class="text2">
+                            <a href="">¿Su usuario está bloqueado o inactivo?</a>
+                        </div>
+                    </div>
+                    <div class="buttones text-center d-flex justify-content-between">
+                        <button class="btn" id="registrer"
+                            style="font-size: 25px; border-color: #39A900; height: 70px;">Registrarse</button>
+                        <button class="btn btn-primary"
+                            style="font-size: 25px; background-color: #39A900; border-color: #39A900;">Iniciar
+                            sesion</button>
+                    </div>
+                    <div class="">
+                        <a class="olvido-contrasena" href="../forgot/forgot.html">Olvido su contraseña?</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img class="imagen-login" src="../resources/images/_image_login.png" alt="Imagen login">
+                <div class="col-md-5 second">
+                    <div>
+                        <img src="../resources/images/_image_login.png" alt="Login image" class="image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="login.js"></script>
+</body>*/
